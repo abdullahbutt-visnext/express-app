62dda1bc9d60f51bf84c141032909a971052118d
abdullahbutt-visnext <abdullah.butt@visnext.net>
2026-07-28 16:24:32 +0500

    setup the database and created the models for the project

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(` Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server.");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
